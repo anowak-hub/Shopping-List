959c8c97bfc435f24d8c1e5a9583036a0e3292a5
Andrew Nowak <anowak4320@stu.d214.org>
2021-03-30 15:24:37 -0500

    Made items and a counter

--- Shopping List/ViewController.swift
+++ Shopping List/ViewController.swift
@@ -8,12 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+// MARK: - Outlets, Variables, Constants, etc.
+   
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    @IBOutlet weak var itemTextField: UITextField!
+    
+    var items: [String] = []
+    
+// MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.dataSource = self
+        
+        items.append("Bacon")
+        items.append("Eggs")
+        items.append("Milk")
     }
+// MARK: - Functions, Actions, etc.
+    
+    @IBAction func whenBarButtonPressed(_ sender: UIBarButtonItem) {
+    
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+
+
+
 
 
 }
